9acbf37412b8a4764a053175f51f8f73d72ecdc1
ht <ht@ht.ht>
2023-02-12 01:00:52 +0800

    add code

--- pin/typewalker/json_bench_test.go
+++ pin/typewalker/json_bench_test.go
@@ -2,8 +2,11 @@ package typewalker
 
 import (
 	"encoding/json"
+	jsoniter "github.com/json-iterator/go"
 	"testing"
 )
+
+var jsonx = jsoniter.ConfigCompatibleWithStandardLibrary
 
 func BenchmarkSmallStruct(b *testing.B) {
 	type S struct {
@@ -25,6 +28,19 @@ func BenchmarkSmallStruct(b *testing.B) {
 	})
 
 	b.Run("Std.Marshal", func(b *testing.B) {
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				v := S{
+					X: 3,
+					Y: "hello-world",
+				}
+				json.Marshal(v)
+			}
+		})
+	})
+
+	b.Run("jsoniter.Marshal", func(b *testing.B) {
 		b.ResetTimer()
 		b.RunParallel(func(pb *testing.PB) {
 			for pb.Next() {
