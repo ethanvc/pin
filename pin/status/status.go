253b53771836a28ae49c5b052548fd15bab6ae27
ht <ht@ht.ht>
2023-02-12 21:27:24 +0800

    add code

--- pin/status/status.go
+++ pin/status/status.go
@@ -1,1 +1,11 @@
 package status
+
+import "github.com/ethanvc/pin/pin/status/code"
+
+type Status struct {
+	codeVal     code.Code
+	eventVal    string
+	subEventVal string
+	msgVal      string
+	pcVal       uintptr
+}
